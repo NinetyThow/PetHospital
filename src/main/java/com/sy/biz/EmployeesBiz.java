294ca23b903b4505af8266ef9afd5baf8de14b7c
weidonglai <421564000@qq.com>
2018-11-30 09:06:35 +0800

    wdl的java

--- src/main/java/com/sy/biz/EmployeesBiz.java
+++ src/main/java/com/sy/biz/EmployeesBiz.java
@@ -1,7 +1,14 @@
 package com.sy.biz;
 
 import com.sy.pojo.Employees;
+import com.sy.pojo.PageBean;
+import com.sy.pojo.Specialties;
+import com.sy.pojo.Vets;
 
 public interface EmployeesBiz {
-    public Employees login(Employees emp);
+    Employees login(Employees emp);
+
+    PageBean queryVetsByPage(int pageSize,int pageCode);
+
+    void addWholeVet(Vets vets);
 }
